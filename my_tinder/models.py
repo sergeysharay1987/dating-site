cf1f69bb83300a3151bf7d24a35331309b434829
serg <sergeysharay1987@gmail.com>
2022-02-07 15:29:19 +0300

    Создана модель Client в файле my_tinder/models.py

--- my_tinder/models.py
+++ my_tinder/models.py
@@ -1,4 +1,16 @@
-
+from django.db.models import Model, CharField, ImageField, EmailField
 
 # Create your models here.
 
+
+class Client(Model):
+    MALE = 'М'
+    FEMALE = 'Ж'
+    GENDERS = ((MALE, 'Мужчина'),
+               (FEMALE, 'Женщина'))
+
+    avatar = ImageField(verbose_name='Фото', upload_to='photos/%Y/%m/%d')
+    gender = CharField(verbose_name='Пол', max_length=7, choices=GENDERS)
+    first_name = CharField(verbose_name='Имя', max_length=50)
+    second_name = CharField(verbose_name='Фамилия', max_length=50)
+    email = EmailField(verbose_name='Электронная почта', unique=True)
