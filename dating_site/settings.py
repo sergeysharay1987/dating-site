c7dc9090827c57e5979fcaafdeafcd5e904fa133
serg <sergeysharay1987@gmail.com>
2022-02-12 00:59:41 +0300

    В файле my_tinder/forms.py изменены виджеты полей В файле dating_site/setting.py изменена константа MEDIA_ROOT В файле my_tinder/views.py изменена функция create_client, изменён файл .gitignore Изменён файл .gitignore, добвлена папка media, добвлена папка dating_site/.idea В файле dating_site/templates/base.html изменён атрибут class в контейнере div

--- dating_site/settings.py
+++ dating_site/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- my_tinder/views.py
+++ my_tinder/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from .forms import CreateClientForm
+
+
 # Create your views here.
 
 
 def create_client(request):
     if request.method == 'GET':
+
         form = CreateClientForm()
         return render(request, 'my_tinder/create_client.html', {'form': form})
+
     elif request.method == 'POST':
+
         bound_form = CreateClientForm(request.POST, request.FILES)
         if bound_form.is_valid():
 
             data = bound_form.cleaned_data
-            bound_form = CreateClientForm(data)
+            bound_form = CreateClientForm(data, request.FILES)
             bound_form.save()
+            return render(request, 'my_tinder/create_client.html', {'form': bound_form})
+        else:
+
             return render(request, 'my_tinder/create_client.html', {'form': bound_form})
