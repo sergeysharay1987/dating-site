0d44fbf2fa3223b02ca9ad91d62b33ed6f4881f2
serg <sergeysharay1987@gmail.com>
2022-03-01 22:07:10 +0300

    Изменён .gitignore

--- my_tinder/forms.py
+++ my_tinder/forms.py
@@ -1,8 +1,25 @@
 from django.contrib.auth.forms import UserCreationForm
+from django.forms import CharField
+from django import forms
 from .models import Client
 
 
 class CreateClientForm(UserCreationForm):
+    password1 = CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
     class Meta:
         model = Client
-        fields = ['avatar', 'gender', 'first_name', 'last_name', 'email']
+        fields = ['avatar',
+                  'gender',
+                  'username',
+                  'last_name',
+                  'email',
+                  'password1',
+                  'password2']
+
+        widgets = {'username': forms.TextInput(attrs={'class': 'form-control'}),
+                   'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+                   'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+                   'email': forms.TextInput(attrs={'class': 'form-control'})
+                   }
