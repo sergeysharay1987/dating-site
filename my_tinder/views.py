e7a41bdf51fa5dc06bf0d521bd5f5180ec0c3ed5
serg <sergeysharay1987@gmail.com>
2022-03-07 12:49:16 +0300

    В файде my_tinder/views.py внесены изменения в функцию create_client

--- my_tinder/views.py
+++ my_tinder/views.py
@@ -1,8 +1,9 @@
+from PIL import Image
 from django.shortcuts import render
 
 from dating_site.settings import MEDIA_ROOT
 from .forms import CreateClientForm
-
+from django.core.files.uploadedfile import InMemoryUploadedFile, SimpleUploadedFile
 
 # Create your views here.
 from .put_watermark import put_watermark
@@ -18,11 +19,28 @@ def create_client(request):
 
         bound_form = CreateClientForm(request.POST, request.FILES)
         if bound_form.is_valid():
-            print(request.FILES)
-            if request.FILES:
-                request.FILES['avatar'] = put_watermark(request.FILES['avatar'], f'{MEDIA_ROOT}/watermark.jpg')
-            data = bound_form.cleaned_data
-            bound_form = CreateClientForm(data, request.FILES)
+            #print(request.FILES)
+            #if request.FILES:
+            image_field: InMemoryUploadedFile = bound_form.cleaned_data['avatar']
+            print(f'image_field.file: {image_field.file}')
+            print(f'type(image_field): {type(image_field)}')
+
+            image = Image.open(image_field, mode='r')
+            print(f'image_location: {image.format}')
+            print(f'image: {image}')
+            #print(f'image_field: {image_field}')
+            #watermarked_image = put_watermark(image_field, f'{MEDIA_ROOT}/watermark.jpg')
+            watermarked_image = put_watermark(image, f'{MEDIA_ROOT}/watermark.jpg')
+            last_name = bound_form.cleaned_data['last_name']
+            gender = bound_form.cleaned_data['gender']
+            email = bound_form.cleaned_data['email']
+            password1 = bound_form.cleaned_data['password1']
+            password2 = bound_form.cleaned_data['password2']
+            data = {'gender': gender, 'last_name': last_name, 'email': email, 'password1':password1, 'password2': password2}
+            #data = bound_form.cleaned_data
+            #print(f'{image.filename}')
+            file_data = {'avatar': SimpleUploadedFile(watermarked_image)}
+            bound_form = CreateClientForm(data, file_data)
             bound_form.save()
             return render(request, 'my_tinder/create_client.html', {'form': bound_form})
         else:
