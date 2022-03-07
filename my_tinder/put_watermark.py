20b5f1dd1297ab81890a21201fd821ded650a546
serg <sergeysharay1987@gmail.com>
2022-03-07 12:48:29 +0300

    В файде my_tinder/put_watermark.py внесены изменения в функцию put_watermark

--- my_tinder/put_watermark.py
+++ my_tinder/put_watermark.py
@@ -1,9 +1,10 @@
 from PIL import Image
 from dating_site.settings import MEDIA_ROOT
 import datetime
+import os
 
 
-def put_watermark(image_path, watermark_path):
+'''def put_watermark(image_path, watermark_path):
 
     base_image = Image.open(image_path)
     watermark_image = Image.open(watermark_path).convert('RGBA')
@@ -22,6 +23,32 @@ def put_watermark(image_path, watermark_path):
     watermark_image.putdata(new_data)
     base_image.paste(watermark_image, box=(0, 150), mask=watermark_image)
     #base_image.save(f'{MEDIA_ROOT}/photos/%Y/%m/%d/new_Screenshot_{datetime.datetime.now()}.jpg')
-    base_image.save(f'{image_path}')
+    base_image.save(f'{image_path}')'''
+
+def put_watermark(image, watermark_path):
+
+    #base_image = Image.open(image_path)
+    watermark_image = Image.open(watermark_path).convert('RGBA')
+    datas = watermark_image.getdata()
+    new_data: list = []
+
+    for item in datas:
+
+        if 50 <= item[0] <= 255 and 50 <= item[1] <= 255 and 50 <= item[1] <= 255:
+
+            new_data.append((item[0], item[1], item[1], 0))
+        else:
+
+            new_data.append((item[0], item[1], item[1], 150))
+
+    watermark_image.putdata(new_data)
+    image.paste(watermark_image, box=(0, 150), mask=watermark_image)
+
+    #base_image.save(f'{MEDIA_ROOT}/photos/%Y/%m/%d/new_Screenshot_{datetime.datetime.now()}.jpg')
+
+   # image.save(f'{image.filename}', format='JPEG')
+    image.save(f'{MEDIA_ROOT}/photos/new_image.jpg', format='JPEG')
+    print(f'watermarked_image___________________: {image}')
+    return image
 
 
