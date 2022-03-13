178f2d899fbc270aa5df5f29b159bbc422ca54c1
serg <sergeysharay1987@gmail.com>
2022-03-14 00:14:44 +0300

    В файле my_tinder/put_watermark.py внечены изменения в фукнцию put_watermark

--- my_tinder/put_watermark.py
+++ my_tinder/put_watermark.py
@@ -1,3 +1,4 @@
+from PIL.Image import Image
 from PIL import Image
 from io import BytesIO
 from PIL.ImageFile import ImageFile
@@ -33,21 +34,30 @@ from PIL.JpegImagePlugin import JpegImageFile
 def put_watermark(image: Image, watermark_path: str):
     # base_image = Image.open(image_path)
     #print(f'image_in_put_watermark_function{image}')
-    watermark_image = Image.open(watermark_path).convert('RGBA')
+    image = image.convert('RGB')
+    watermark_image = Image.open(watermark_path).convert('RGB')
+    watermark = Image.new('RGBA', watermark_image.size, (255, 255, 255, 255))
+    #    pos_watermark = (back_watermark.size[0]-720, back_watermark.size[1]-750)
+    # print(pos_watermark)
+    watermark.paste(watermark_image)
     datas = watermark_image.getdata()
+    #watermark.show()
     new_data: list = []
 
     for item in datas:
 
-        #if 150 <= item[0] <= 255 and 150 <= item[1] <= 255 and 150 <= item[1] <= 255:
         if item[0] != 255 and item[1] != 255 and item[1] != 255:
 
-            new_data.append((item[0], item[1], item[2], 100))
+            new_data.append((item[0], item[1], item[2], 0))
         else:
 
             new_data.append((item[0], item[1], item[2], 50))
-    watermark_image.putdata(new_data)
-    image.paste(watermark_image, box=(0, 150), mask=watermark_image)
+    watermark.putdata(new_data)
+    watermark.show()
+    watermark = watermark.resize((watermark.size[0] // 2, watermark.size[1] // 2))
+    pos_water = (image.size[0] - watermark.size[0], image.size[1] - watermark.size[1])
+    image.paste(watermark, box=pos_water, mask=watermark)
+    image.show()
     byte_io = BytesIO()
     byte_io.seek(0)
     image = image.save(byte_io, 'PNG')
