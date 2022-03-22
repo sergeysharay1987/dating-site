9e79d88bc8432b8333b0da10915f3adb90dc052d
serg <sergeysharay1987@gmail.com>
2022-03-22 23:34:10 +0300

    В файле my_tinder/put_watermark изменена логика функции put_watermark

--- my_tinder/put_watermark.py
+++ my_tinder/put_watermark.py
@@ -51,35 +51,48 @@ def put_watermark(base_image: Image, watermark_path: str):
     # print(f'image_in_put_watermark_function{image}')
     base_image = base_image.convert('RGB')
     watermark_image = Image.open(watermark_path).convert('RGBA')
-    #watermark = Image.new('RGBA', watermark_image.size, (128, 128, 128, 0))
-
-    #    pos_watermark = (back_watermark.size[0]-720, back_watermark.size[1]-750)
-    # print(pos_watermark)
-    #watermark.paste(watermark_image)
+    middle_pixels = []
+    #base_image = Image.open(image_path).convert('RGB')
+    watermark_image = Image.open(watermark_path).convert('RGBA')
     datas = watermark_image.getdata()
-    # watermark.show()
-    new_data: list = []
+    for item in datas:  # получаем значения каналов red, green, blue, aplpha
+        middle_pixel = (item[0] + item[1] + item[2]) // 3  # ищем среднее-арифметическое каналов (red+green+blue)/3
+        # alpha канал не учитываем, так как он отвечает за
+        # прозрачность
+        if middle_pixel != 0:  # если среднее-арифметическое не равно 0 (если цвет - не чёрный), то добавляем его в
+            # список
+            middle_pixels.append(middle_pixel)
+    set_middle_values = set(middle_pixels)  # преобразуем список в множество (убираем повторяющиеся значения),
+    # чтобы узнать какие цвета присутствуют в изображении
+    number_occurrences = []  # создаём список вхождений, в него будем класть кортеж из 2-х элементов:
+    # среднее-арифметическое (red+green+blue)/3 и кол-во вхождений этого среднего-арифметического в изображение
+    for value in set_middle_values:
+        number_occurrences.append((value, middle_pixels.count(value)))  # кладём среднее-арифметическое
+        # (red+green+blue)/3 и кол-во вхождений этого среднего арифметического в изображение
+    number_occurence = max(number_occurrences, key=lambda x: x[1])  # находим среднее-арифметическое наиболее
+    # используемого цвета в изображении,
+    # найдя максимальное число среди кол-ва вхождений
+    main_color = number_occurence[0]  # получаем среднее-арифметическое наиболее часто используемого цвета в изображении
+    new_data: list = []  # создаём список, чтобы класть в него новые значения каналов (red, green, blue,
+    # alpha) пикселей изображения
 
     for item in datas:
-        #if item[0] == 128 and item[1] == 128 and item[2] == 128:
-        if (item[0] != 0 and item[1] != 0 and item[2] != 0):
-        # if item[0] == 0 and item[1] == 0 and item[2] == 0:
-        #if item[0] == 255 and item[1] == 255 and item[2] == 255:
-        #if 250 <= item[0] <= 255 and 250 <= item[1] <= 255 and 250 <= item[2] <= 255:
-            #new_data.append((item[0], item[1], item[2], 150))
-            new_data.append((item[0], item[1], item[2], 150))
-            #new_data.append((0, 0, 0, 0))
-        #else:
-        elif (item[0] == 0 and item[1] == 0 and item[2] == 0) or (item[0] <= 150 or item[1] <= 150 or item[2] <= 150):
-            new_data.append((0, 0, 0, 0))
-            #new_data.append((0, 0, 0, 0))
 
+        if item[0] + item[1] + item[2] / 3 >= main_color:  # если среднее-арифметическое каналов red, green, blue
+            # больше или равно среднему-арифметическому значению наиболее используемого цвета, то делаем цвет
+            # полупрозрачным
+
+            new_data.append((item[0], item[1], item[2], 70))
+        else:  # иначе, делаем цвет
+            # полностью прозрачным
+            new_data.append((item[0], item[1], item[2], 0))
     watermark_image.putdata(new_data)
-    #watermark_image = make_size(base_image, watermark_image)
-    print(f'watermark_image: {watermark_image}')
-    watermark_image.show()
-    base_image.paste(watermark_image, mask=watermark_image)
-    base_image.show()
+
+    watermark_image = watermark_image.resize((watermark_image.size[0] // 2, watermark_image.size[1] // 2))
+
+    base_image.paste(watermark_image,
+                     (base_image.size[0] - watermark_image.size[0], base_image.size[1] - watermark_image.size[1]),
+                     mask=watermark_image)
     byte_io = BytesIO()
     byte_io.seek(0)
     base_image.save(byte_io, 'PNG')
