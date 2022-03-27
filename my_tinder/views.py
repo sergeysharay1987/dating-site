4e53c78e44b41cff69d0e36d919889617efe7691
serg <sergeysharay1987@gmail.com>
2022-03-27 23:20:03 +0300

    Функции create_client и show_client в my_tinder/views.py изменена

--- my_tinder/views.py
+++ my_tinder/views.py
@@ -1,6 +1,6 @@
 from PIL import Image
 from my_tinder.models import CustomUser
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from dating_site.settings import MEDIA_ROOT
 from .forms import CreateClientForm
 from django.core.files.uploadedfile import InMemoryUploadedFile, SimpleUploadedFile
@@ -27,16 +27,7 @@ def create_client(request):
             watermarked_image.seek(0)
             file_data = {'avatar': SimpleUploadedFile(f'image.png', watermarked_image.read(),
                                                       content_type=f'image/png')}
-            last_name = bound_form.cleaned_data['last_name']
-            gender = bound_form.cleaned_data['gender']
-            email = bound_form.cleaned_data['email']
-            password1 = bound_form.cleaned_data['password1']
-            password2 = bound_form.cleaned_data['password2']
-            data = {'gender': gender,
-                    'last_name': last_name,
-                    'email': email,
-                    'password1': password1,
-                    'password2': password2}
+            data = bound_form.cleaned_data
             bound_form = CreateClientForm(data, file_data)
             bound_form.save()
             return render(request, 'my_tinder/create_client.html', {'form': bound_form})
@@ -46,8 +37,8 @@ def create_client(request):
 
 
 def show_client(request, id):
-    client: CustomUser = CustomUser.objects.get(id=id)
 
+    client: CustomUser = get_object_or_404(CustomUser, id=id)
     client_info = {'avatar': client.avatar,
                    'gender': client.gender,
                    'first_name': client.first_name,
