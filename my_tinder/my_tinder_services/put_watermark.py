9eccbfbe2138d8fc4d2b480585251d0e5b29ac48
serg <sergeysharay1987@gmail.com>
2022-04-09 08:33:11 +0300

    Функция get_datas добавлена

--- my_tinder/my_tinder_services/put_watermark.py
+++ my_tinder/my_tinder_services/put_watermark.py
@@ -59,6 +59,58 @@ import numpy as np
 def get_middle_value(tup):
     return (tup[0] + tup[1] + tup[2]) // 3
 
+def get_datas(image_path):
+    image = Image.open(image_path)
+    #datas_by_by_getdata = image.getdata()
+    #image_array_by_getdata = np.array(datas_by_by_getdata)
+    image_by_np_array = np.array(image)
+    # print(f'image.size: {image.size}')
+    # print(f'image_array_by_getdata.shape: {image_array_by_getdata.shape}')
+    # print(f'image_array_by_getdata.dtype: {image_array_by_getdata.dtype}')
+    # print(f'image_array_by_getdata.size: {image_array_by_getdata.size}')
+    # print(f'image_array_by_getdata.itemsize: {image_array_by_getdata.itemsize}')
+    print()
+    print(f'image_by_np_array.shape: {image_by_np_array.shape}')
+    print(f'image_by_np_array.dtype: {image_by_np_array.dtype}')
+    print(f'image_by_np_array.size: {image_by_np_array.size}')
+    print(f'image_by_np_array.itemsize: {image_by_np_array.itemsize}')
+    middle_pixels = image_by_np_array.mean(axis=2, dtype='uint16')
+    #print(f'middle_pixels.shape: {middle_pixels.shape}')
+    middle_pixels.shape = (-1, 1)
+    print(f'middle_pixels.shape: {middle_pixels.shape}')
+    unique_middle_pixels, counts = np.unique(middle_pixels, return_counts=True)
+
+    unique_middle_pixels = unique_middle_pixels[1:]
+    counts = counts[1:]
+    max_counts_of_main_color = np.max(counts)
+    index_of_main_usable_color = np.where(counts == max_counts_of_main_color)[0][0]
+    main_color = unique_middle_pixels[index_of_main_usable_color]
+    print(f'index_of_main_usable_color: {index_of_main_usable_color}')
+    image_by_np_array.shape = (-1, 3)
+
+    print(f'image_by_np_array[:,:]: {image_by_np_array[:, 2]}')
+    alpha_channel = np.where(middle_pixels >= main_color, 70, 0)
+    alpha_channel.dtype = 'uint8'
+    alpha_channel.shape = -1,1
+    print(f'alpha_channel,shape: {alpha_channel.shape}')
+
+    # for value in alpha_channel:
+    #     print(value)
+    new_datas_with_alpha = np.concatenate((image_by_np_array, alpha_channel), axis=1, dtype='uint8')
+    print(f'new_datas_with_alpha.shape: {new_datas_with_alpha.shape}')
+
+    image_for_putting_new_datas = Image.new(size=image.size, mode='RGB')
+    #image_for_putting_new_datas.putdata(new_datas_with_alpha)
+    #image_from_np_array.show()
+    image_for_putting_new_datas.show()
+
+
+
+
+
+
+
+
 
 def put_watermark(base_image: Image, watermark_path: str) -> BytesIO:
     """Накладывает водяной знак на изображение. В качестве водяного знака
