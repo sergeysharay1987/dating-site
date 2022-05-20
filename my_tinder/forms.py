d090b9e09e019b5e3f86c3cddcced7f1c98dce44
serg <sergeysharay1987@gmail.com>
2022-05-20 13:22:22 +0300

    В my_tinder/forms.py добавлена форма, унаследованная от модели CustomUser, но содержащая только кнопки (Нравится/Не нравится), для того чтобы была возможность поставить симпатию другому участнику

--- my_tinder/forms.py
+++ my_tinder/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.forms import CharField
+from django.forms import CharField, ModelForm
 from django import forms
 from .models import CustomUser
 
@@ -17,8 +17,21 @@ class CreateClientForm(UserCreationForm):
                   'password1',
                   'password2']
 
-        widgets = {#'username': forms.TextInput(attrs={'class': 'form-control'}),
-                   'first_name': forms.TextInput(attrs={'class': 'form-control'}),
-                   'last_name': forms.TextInput(attrs={'class': 'form-control'}),
-                   'email': forms.TextInput(attrs={'class': 'form-control'})
-                   }
+        widgets = {  # 'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.TextInput(attrs={'class': 'form-control'})
+        }
+
+
+class ToLikeClientForm(ModelForm):
+    like = CharField(label='', max_length=100, widget=forms.TextInput(
+        attrs={'class': 'btn btn-light active', 'type': 'submit', 'value': 'Нравится'}))
+    dislike = CharField(label='', max_length=100,
+                        widget=forms.TextInput(
+                            attrs={'class': 'btn btn-light active', 'type': 'submit', 'aria-pressed':'True', 'value': 'Не нравится'}))
+
+    class Meta:
+        model = CustomUser
+        fields = ['like']
+
