5cf78815dce1baa9f9304d018419f6c9cce0d0dc
serg <sergeysharay1987@gmail.com>
2022-05-25 10:32:36 +0300

    В my_tinder/static/my_tinder/js/main.js добавлена логика для отправки ajax-запроса

--- my_tinder/static/my_tinder/js/main.js
+++ my_tinder/static/my_tinder/js/main.js
@@ -1,6 +1,25 @@
-$(document).ready(function){
+$(document).ready(function(){
 
-    $('id_like').addEventListener('click', function(){
-    this.classList.toggle('activated');
-    )}
+    $("z").mouseover(function(){
+        $.ajax({
+            url: $('form').attr('action'),
+            type: 'get',
+            data: {
+                button_text: $(this).attr('value')
+            },
+            success: function(button_text){
+                alert('Congrats!' + button_text)
+                console.log(button_text)
+            }
+        });
+    });
+
+
+
+    /*$('#id_like').mouseover(function(){
+        //let attr_name = $('html body div.container.mt-5 div.row div.col-7.offset-md-3 h1').text()
+        let form_attr_action = $('form').attr('action')
+        //alert(attr_name)
+        alert(form_attr_action)
+    });*/
 });
