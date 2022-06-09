f616d3d99ef2dd5e747f4a7e09b7d3f6cd0c3ef9
serg <sergeysharay1987@gmail.com>
2022-06-09 22:24:22 +0300

    В my_tinder/templatetags/verbose_names.py добавлен шаблонный тег для получения понравившихся пользователей

--- my_tinder/templatetags/verbose_names.py
+++ my_tinder/templatetags/verbose_names.py
@@ -1,11 +1,19 @@
 from django import template
+from ..models import CustomUser
 
 register = template.Library()
 
 
 @register.simple_tag
-def get_verbose_field_name(instance, field_name):
+def get_verbose_field_name(instance: CustomUser, field_name: str):
     """
-    Возвращает verbose_name для каждого поля.
+    Возвращает verbose_name для каждого поля модели.
     """
     return instance._meta.get_field(field_name).verbose_name.title()
+
+
+def get_liked_users(instance: CustomUser):
+    """
+    Возвращает QuerySet дочерней модели.
+    """
+    return instance.customuser_set.all()
