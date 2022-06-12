13d73c809a6d72f36dd390ea7ebe820018443445
serg <sergeysharay1987@gmail.com>
2022-06-12 08:46:55 +0300

    В my_tinder/static/my_tinder/js/main.js добавлены новые переменные text_button, class_button, а также условие для дальнейшей смены цветов кнопок 'Нравится' 'Не нравится' при нажатии на одну из них

--- my_tinder/static/my_tinder/js/main.js
+++ my_tinder/static/my_tinder/js/main.js
@@ -1,14 +1,23 @@
 $(document).ready(function(){
     // Получаем значение csrf_token //
     var csrf = $('input[name=csrfmiddlewaretoken]').val();
+    var text_button = $('button').text()
+    var class_button = $('button').attr('class')
 
-        $('button:contains("Нравится")').click(function(){
+        if (text_button ==="Нравится" || "Не нравится"){
+            console.log(text_button)
+            text_button.attr('class', )
+
+            }
+
+
+        $('button:contains("равится")').click(function(){
             $(this).attr('class', 'btn btn-primary')
             alert('Событие click')
 
         $.ajax({
         // Получаем url из атрибута data-url тега button //
-            url: $('button:contains("btn btn")').attr('data-url'),
+            url: $('button:contains("равится")').attr('data-url'),
             type: 'post',
             data: {
             // Получаем email другого участника из атрибута id тега h1//
