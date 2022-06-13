a26de2a2bf596bcf97402a372c5cf2c5af1b93b2
serg <sergeysharay1987@gmail.com>
2022-06-13 23:56:33 +0300

    В my_tinder/static/my_tinder/js/main.js изменена логика отправки ajax запроса, также отправка ajax запроса вынесена в отдельную функцию

--- my_tinder/static/my_tinder/js/main.js
+++ my_tinder/static/my_tinder/js/main.js
@@ -1,33 +1,58 @@
 $(document).ready(function(){
     // Получаем значение csrf_token //
     var csrf = $('input[name=csrfmiddlewaretoken]').val();
-    var text_button = $('button').text()
-    var class_button = $('button').attr('class')
-
-        if (text_button ==="Нравится" || "Не нравится"){
-            console.log(text_button)
-            text_button.attr('class', )
-
-            }
+    var btn = $('button:contains("равится")');
+    var like_btn = $('button:contains("Нравится")')
+    var unlike_btn = $('button:contains("Не нравится")')
 
 
-        $('button:contains("равится")').click(function(){
-            $(this).attr('class', 'btn btn-primary')
-            alert('Событие click')
-
-        $.ajax({
+    function sendAjax(method='post', btnType){
+          $.ajax({
         // Получаем url из атрибута data-url тега button //
             url: $('button:contains("равится")').attr('data-url'),
-            type: 'post',
+            type: method,
             data: {
             // Получаем email другого участника из атрибута id тега h1//
                 other_client_email: $('h1').attr('id'),
-                csrfmiddlewaretoken: csrf
+                csrfmiddlewaretoken: csrf,
+                btnType: btnType
             },
             success: function(response) {
-                console.log($('#is_like').attr('class'));
-
-            }
+                console.log($(response.request));}
         });
+    }
+        /*$('button').click(function(){
+            if $('button:')
+            if (button.text() ==='Нравится'){
+                $(button).attr('class', 'btn btn-primary')
+                $('button').text('Не нравится').attr('class', 'btn btn-light')
+                }
+            else if (button.text('Не нравится')){
+                $(this).attr('class', 'btn btn-primary')
+                $('button').text('Нравится').attr('class', 'btn btn-light')
+
+             }
+            }*/
+
+
+        /*$(btn).click(function(){
+            if (btn.text() === 'Нравится'){
+                $(btn).attr('class', 'btn btn-primary');
+                $('button:contains("Не нравится")'.attr('class', 'btn btn-light'));
+                }*/
+
+        $(unlike_btn).click(function(){
+            $(unlike_btn).attr('class', 'btn btn-success');
+            $(like_btn).attr('class', 'btn btn-light');
+            sendAjax(unlike_btn)})
+
+
+        $(like_btn).click(function(){
+            $(unlike_btn).attr('class', 'btn btn-light');
+            $(like_btn).attr('class', 'btn btn-success');
+            sendAjax(like_btn)
+
+
+
     });
 });
