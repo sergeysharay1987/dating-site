6009982f4dcc03fbf3de948f4f36a15ee89525c4
serg <sergeysharay1987@gmail.com>
2022-07-13 23:06:34 +0300

    Удалены неиспользуемые импорты .Изменена логика функции test_user_creation_form. Добавлен вариант функции test_registration (он закомментирован), в котором измненен способ отправки изображения в post запросе

--- my_tinder/tests/test_views.py
+++ my_tinder/tests/test_views.py
@@ -1,31 +1,14 @@
-import os
 from io import BytesIO
 from dating_site.settings import BASE_DIR
 from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.http import HttpResponse
 from django.test import Client
-# Create your tests here.
 import pytest
 from django.urls import reverse
 from PIL import Image
-
-from dating_site import settings
 from my_tinder.apps import MyTinderConfig
 from my_tinder.forms import CreateClientForm
-
-# im = Image.open('image.png')
-# byte_io = BytesIO()
-# byte_io.seek(0)
-# im.save(byte_io, 'PNG')
-
-#
-# @pytest.fixture(scope='session')
-# def django_db_setup():
-#     settings.DATABASES['test_db'] = {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
 
 data = {
     'gender': 'M',
@@ -34,46 +17,60 @@ data = {
     'password1': 'qwerty1000',
     'password2': 'qwerty1000'}
 
-# path = f'/dating_site/my_tinder/tests/image.png'
-path = f'/{BASE_DIR}/{MyTinderConfig.name}/tests/image.png'
+image_path = f'/{BASE_DIR}/{MyTinderConfig.name}/tests/image.png'
 
 
 def get_file_data(path: str):
     image = Image.open(path)
     bytes_io = BytesIO()
     image.save(bytes_io, 'png')
-    #print(f'cwd: {os.getcwd()}')
-
     return bytes_io.getvalue()
 
 
-file_data = get_file_data(path)
-file_data = {'avatar': SimpleUploadedFile('image.png', file_data,
-                                          content_type=f'image/png')}
+file_data = get_file_data(image_path)
+file_data_form = {'avatar': SimpleUploadedFile('image.png', file_data,
+                                               content_type=f'image/png')}
 
 
-'''@pytest.mark.django_db
+@pytest.mark.django_db
 def test_user_creation_form():
-    print(path)
-    bound_form = CreateClientForm(data, file_data)
+    print(image_path)
+    bound_form = CreateClientForm(data, file_data_form)
     if bound_form.is_valid():
         bound_form.save()
+        users = get_user_model().objects.all()
+        assert users.count() == 1
     else:
-        return 0
-    users = get_user_model().objects.all()
-    assert users.count() == 1'''
+        return bound_form.errors
 
 
 @pytest.mark.django_db
 def test_registration():
     client = Client()
-    with open(path, 'rb') as fp:
+    with open(image_path, 'rb') as fp:
         resp: HttpResponse = client.post(reverse('registration'), {'avatar': fp,
+                                                                   'gender': 'M',
+                                                                   'last_name': 'Ivanov',
+                                                                   'email': 'Ivanov_1000@gmail.com',
+                                                                   'password1': 'qwerty1000',
+                                                                   'password2': 'qwerty1000'})
+        if resp.status_code != 200:
+            print(f'response: {resp.content}')
+
+        assert get_user_model().objects.get(email='Ivanov_1000@gmail.com')
+
+
+img = BytesIO(file_data)
+img.name = 'image.png'
+
+'''@pytest.mark.django_db
+def test_registration():
+    client = Client()
+    resp: HttpResponse = client.post(reverse('registration'), {'avatar': img,
                                                                'gender': 'M',
                                                                'last_name': 'Ivanov',
                                                                'email': 'Ivanov_1000@gmail.com',
                                                                'password1': 'qwerty1000',
                                                                'password2': 'qwerty1000'}
                                      )
-    assert get_user_model().objects.get(email='Ivanov_1000@gmail.com')
-
+    assert get_user_model().objects.get(email='Ivanov_1000@gmail.com')'''
