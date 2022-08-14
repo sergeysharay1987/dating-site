ddf46750b2b0c041515f3df8162bc5c586cd8a2b
serg <sergeysharay1987@gmail.com>
2022-08-14 23:42:08 +0300

    Добавлены представления DetailClientApiView и ListClientsApiView

--- my_tinder/views.py
+++ my_tinder/views.py
@@ -1,18 +1,15 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView
 from django.core.exceptions import ObjectDoesNotExist
-from django.http import HttpResponseRedirect
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login, authenticate, logout
-from django.contrib.auth.forms import AuthenticationForm
+from .serializers import CustomUserSerializer
 from django.core.files.uploadedfile import InMemoryUploadedFile, SimpleUploadedFile
-from django.urls import reverse, reverse_lazy
+from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views import View
-from django.views.generic import DetailView, ListView
 
 from dating_site.settings import BASE_DIR
-from django.db.models import QuerySet
 from .apps import MyTinderConfig
 from PIL import Image
 from my_tinder.models import CustomUser
@@ -58,32 +55,10 @@ def registration(request):
             return render(request, 'my_tinder/registration.html', {'form': bound_form})
 
 
-@method_decorator(decorator=login_required, name='get')
-class ClientPageView(DetailView):
-    model = CustomUser
-    pk_url_kwarg = 'id'
-    template_name = 'my_tinder/client_page.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        print(f'context: {type(context["object"])}')
-        client_info = {'avatar': context['object'].avatar,
-                       'gender': context['object'].gender,
-                       'first_name': context['object'].first_name,
-                       'last_name': context['object'].last_name}
-        client_email = context['object'].email
-        auth_user_id = context['object'].id
-        context['client_info'] = client_info
-        context['client_email'] = client_email
-        context['auth_user_id'] = auth_user_id
-        return context
-
-    def get(self, request, *args, **kwargs):
-        self.object = self.get_object()
-        context = self.get_context_data(object=self.object)
-        if request.user.id != context['auth_user_id']:
-            return redirect('client_page', id=request.user.id)
-        return self.render_to_response(context)
+class DetailClientApiView(RetrieveAPIView):
+    serializer_class = CustomUserSerializer
+    lookup_field = 'id'
+    queryset = CustomUser.objects.all()
 
 
 class LoginClient(LoginView):
@@ -99,43 +74,9 @@ def logout_client(request):
     return redirect('login')
 
 
-# @login_required()
-# def clients_page(request, id):
-#     if request.method == 'GET':
-#         if id == request.user.id:
-#
-#             auth_user = CustomUser.objects.get(id=id)
-#             other_clients: QuerySet = CustomUser.objects.all()
-#             other_clients = other_clients.exclude(id=auth_user.id)
-#             context = {'id': id,
-#                        'other_clients': other_clients}
-#             return render(request, 'my_tinder/clients_page.html', context=context)
-#         else:
-#             return redirect('watch_clients', id=request.user.id)
-
-
-@method_decorator(decorator=login_required, name='get')
-class ListClientsView(ListView):
-    model = CustomUser
-    template_name = 'my_tinder/clients_page.html'
-    pk_url_kwarg = 'id'
-    context_object_name = 'other_clients'
-
-    def get_queryset(self):
-        auth_user_id = self.request.user.id
-        queryset = super().get_queryset()
-        queryset.exclude(pk=auth_user_id)
-        return queryset
-
-    def get(self, request, *args, **kwargs):
-        self.object_list = self.get_queryset()
-        print(f'request: {request.session["_auth_user_id"]}')
-        context = self.get_context_data()
-        print(f'context: {context[""]}')
-        if request.user.id != request.session["_auth_user_id"]:
-            return redirect('watch_clients', id=request.user.id)
-
-        return self.render_to_response(context)
+class ListClientsApiView(ListAPIView):
+    serializer_class = CustomUserSerializer
+    queryset = CustomUser.objects.all()
 
 
 # Вьюха для просмотра подробной информации о другом участнике
