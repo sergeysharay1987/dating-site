e0d13f627dce074b6a970ecf9d0659dc2dc78506
serg <sergeysharay1987@gmail.com>
2022-09-12 23:25:03 +0300

    Добавлен {trailing_slash}

--- my_tinder/routers.py
+++ my_tinder/routers.py
@@ -7,35 +7,35 @@ class CustomRouter(SimpleRouter):
     """
     routes = [
         Route(
-            url=r'^{prefix}/create$',
+            url=r'^{prefix}/create{trailing_slash}$',
             mapping={'post': 'create'},
             name='{basename}-create',
             detail=False,
             initkwargs={'suffix': 'List'}
         ),
         Route(
-            url=r'^{prefix}$',
+            url=r'^{prefix}/$',
             mapping={'get': 'list'},
             name='{basename}-list',
             detail=False,
             initkwargs={'suffix': 'List'}
         ),
         Route(
-            url=r'^{prefix}/{lookup}$',
+            url=r'^{prefix}/{lookup}{trailing_slash}$',
             mapping={'get': 'retrieve'},
             name='{basename}-detail',
             detail=True,
             initkwargs={'suffix': 'Detail'}
         ),
         Route(
-            url=r'^{prefix}/{lookup}/update$',
+            url=r'^{prefix}/{lookup}/update{trailing_slash}$',
             mapping={'put': 'update'},
             name='{basename}-update',
             detail=True,
             initkwargs={'suffix': 'Update'}
         ),
         Route(
-            url=r'^{prefix}/{lookup}$',
+            url=r'^{prefix}/{lookup}/delete{trailing_slash}$',
             mapping={'delete': 'delete'},
             name='{basename}-detail',
             detail=True,
