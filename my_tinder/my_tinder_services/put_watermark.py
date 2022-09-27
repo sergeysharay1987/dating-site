72897761275e3ae4a77937b73467f3f163934680
serg <sergeysharay1987@gmail.com>
2022-09-27 11:20:59 +0300

    Изменена функция put_watermark

--- my_tinder/my_tinder_services/put_watermark.py
+++ my_tinder/my_tinder_services/put_watermark.py
@@ -1,18 +1,25 @@
+import os
+
 from PIL.Image import Image
 from PIL import Image
 from io import BytesIO
 
+from my_tinder.apps import MyTinderConfig
+from dating_site.settings import BASE_DIR
 
-def put_watermark(base_image: Image, watermark_path: str) -> BytesIO:
+app_name = MyTinderConfig.name  # название приложения
+watermark = 'watermark.png'  # название изображение, содержащее водяной знак
+path_to_watermark = f'{BASE_DIR}/{app_name}/{watermark}'  # путь до изображения, содержащее водяной знак
+
+
+def put_watermark(base_image: Image, watermark_path: str = path_to_watermark) -> BytesIO:
     if base_image.mode != 'RGBA':
-
         base_image = base_image.convert('RGBA')
     watermark_image: Image = Image.open(watermark_path)
     width, heigth = base_image.size
-    watermark_image.thumbnail((width//3, heigth//3))
+    watermark_image.thumbnail((width // 3, heigth // 3))
     destination = (width - watermark_image.size[0], 0)
     base_image.alpha_composite(watermark_image, destination)
     byte_io = BytesIO()
-    byte_io.seek(0)
     base_image.save(byte_io, 'PNG')
     return byte_io
