4db6cc4a53d38cb32b267ccf121de36f4b9d3fd3
serg <sergeysharay1987@gmail.com>
2022-09-30 12:24:05 +0300

    IsUserPkInUrl

--- my_tinder/permissions.py
+++ my_tinder/permissions.py
@@ -1,10 +1,15 @@
 from rest_framework.permissions import BasePermission
+from rest_framework.permissions import SAFE_METHODS
 
 
 class IsUserPkInUrl(BasePermission):
 
+    def has_permission(self, request, view):
+        if view.action =='create':
+            return True
+
     def has_object_permission(self, request, view, obj):
-        if request.user.id == obj.id and view.action == 'update':
+        if view.action in ['update', 'destroy']:
             return True
         return False
 
@@ -14,4 +19,4 @@ class AllowAnyCreate(BasePermission):
     def has_permission(self, request, view):
         if view.action == 'create':
             return True
-        return False
+        # return False
