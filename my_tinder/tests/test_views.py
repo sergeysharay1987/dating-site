dd19ffb13b0923753f5616b6e7be3bd9b9a3dc5e
serg <sergeysharay1987@gmail.com>
2022-10-08 12:57:03 +0300

    Добавлен сериализатор для использвания тестирования, чтобы в поле avatar атрибут url сделать абсолютым

--- my_tinder/tests/test_views.py
+++ my_tinder/tests/test_views.py
@@ -1,6 +1,7 @@
 from io import BytesIO
-
+# from django.shortcuts import reverse
 from rest_framework.reverse import reverse
+from rest_framework.serializers import ModelSerializer
 
 from dating_site.settings import BASE_DIR
 from django.core.files.uploadedfile import SimpleUploadedFile, InMemoryUploadedFile
@@ -12,7 +13,31 @@ from my_tinder.models import CustomUser
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND
 import pytest
 from django.core.management import call_command
+
+from my_tinder.my_tinder_services.put_watermark import put_watermark
 from my_tinder.urls import router
+from my_tinder.serializers import CustomUserSerializer
+from django.db.models import F
+
+
+class CustomUserSerializerTesting(ModelSerializer):
+    DOMAIN = 'http://testserver'
+    class Meta:
+        model = CustomUser
+        fields = ['avatar', 'email', 'gender', 'first_name', 'last_name']
+
+    def save(self, **kwargs):
+        if self.data.get('avatar'):
+            relative_url = self.data['avatar'].url
+            self.data['avatar'].url = CustomUserSerializerTesting.DOMAIN + relative_url
+            print(f'self.data["avatar"].url: {self.data["avatar"].url}')
+            return super().save()
+        if self.validated_data.get('avatar'):
+            base_image: Image = Image.open(self.validated_data['avatar'].file)
+            watermarked_image: BytesIO = put_watermark(base_image)
+            self.validated_data['avatar'].file = watermarked_image
+
+        return super().save()
 
 
 @pytest.fixture(scope='session')
@@ -73,11 +98,20 @@ def user(request):
 
 @pytest.mark.django_db
 def test_list():
+    DOMAIN = 'http://testserver'
+    queryset = CustomUser.objects.all().exclude(id=7)
     api_client = APIClient()
     api_client.force_authenticate(CustomUser.objects.get(id=7))
     api_response = api_client.get(path=reverse(router.urls[0].name), format='json')
-    api_response.render()
+    #queryset = queryset.filter(pk=1).update(avatar=DOMAIN + F('avatar'))
+    # print(f'api_response.get_host(): {api_response.get_host}')
+    # print(f'queryset: {queryset}')
+    #print(f'u_1: {CustomUser.objects.get(id=1).avatar.url}')
+    # api_response.render()
+    serializer = CustomUserSerializerTesting(queryset, many=True)
+    print(f'serializer.data: {serializer.data}')
     assert api_response.status_code == HTTP_200_OK
+    assert (api_response.data) == (serializer.data)
 
 
 @pytest.mark.django_db
@@ -101,13 +135,18 @@ def test_retrieve(user):
         print(f'current_user: {user.id}')
         assert api_response.status_code == HTTP_404_NOT_FOUND
 
-
-@pytest.mark.django_db
-def test_filter():
-    pk = {'pk': 10}
-    api_client = APIClient()
-    api_client.force_authenticate(CustomUser.objects.get(id=7))
-    api_response = api_client.get(reverse(router.urls[0].name), kwargs={'first_name':'Jack'})
-    print(f'reverse(): {api_response.data}')
-    assert api_response.data == CustomUser.objects.filter(first_name__exact='Jack')
-
+# @pytest.mark.django_db
+# def test_filter():
+#     pk = {'pk': 10}
+#     api_client = APIClient()
+#     api_client.force_authenticate(CustomUser.objects.get(id=7))
+#     url = reverse(router.urls[0].name)
+#     api_response = api_client.get(url+'/'+'?first_name=Jack')
+#     #print(f'reverse(): {reverse(viewname=url)}')
+#     assert url == '/my_tinder/list'
+#     assert api_response.data ==
+#     serilizer_data = CustomUserSerializer(api_response, many=True).data
+# print(f'api_response: {api_response.data}')
+# print(f'serilizer_data: {serilizer_data}')
+# assert serilizer_data == api_response.data
+# assert api_response.data == CustomUser.objects.filter(first_name__exact='Jack')
