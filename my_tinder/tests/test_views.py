cf99f2c751c1250030cf42402070903da226d307
serg <sergeysharay1987@gmail.com>
2022-10-29 10:45:59 +0300

    Удалена часть закомментирована кода, неиспользуемые импорты, изменне тест test_retrieve

--- my_tinder/tests/test_views.py
+++ my_tinder/tests/test_views.py
@@ -12,29 +12,11 @@ from rest_framework.test import APIClient
 from my_tinder.models import CustomUser
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_404_NOT_FOUND
 import pytest
-from django.core.management import call_command
-from my_tinder.my_tinder_services.put_watermark import put_watermark
-from my_tinder.urls import router
-from my_tinder.serializers import CustomUserSerializer
 from model_bakery import baker
 
 file_name = 'image.png'
 image_path = f'/{BASE_DIR}/{MyTinderConfig.name}/tests/{file_name}'
 
-
-def get_file_data(path: str):
-    image = Image.open(path)
-    bytes_io = BytesIO()
-    image.save(bytes_io, 'PNG')
-    bytes_io.read()
-    content = bytes_io.getvalue()
-    return content
-
-
-size = Image.open(image_path).size
-file_data = get_file_data(image_path)
-file_data_form = {'avatar': SimpleUploadedFile('image.png', file_data,
-                                               content_type=f'image/{file_name.split(".")[-1]}')}
 
 data = {
     'gender': 'М',
@@ -48,21 +30,10 @@ RETRIEVE_PATH = 'http://testserver/my_tinder/clients/3'
 CREATE_PATH = 'http://testserver/my_tinder/clients/create'
 
 
-@pytest.fixture(autouse=True)
-def use_dummy_cache_backend(settings):
-    settings.MEDIA_URL = 'http://testserver'
-    settings.ALLOWED_HOSTS = ['testserver']
 
 
 # @pytest.fixture(params=[queryset])
 # def user(request):
-#     for user in request.param:
-#         print(f'user: {user}')
-#         return user
-
-
-# @pytest.fixture(params=[queryset])
-# def user(db, request):
 #     for user in request.param:
 #         print(f'user: {user}')
 #         return user
@@ -97,14 +68,9 @@ def list_users(db):
 
 @pytest.mark.django_db
 def test_list(one_user):
-    queryset = CustomUser.objects.all().exclude(id=one_user.id)
     api_client = APIClient()
     api_client.force_authenticate(CustomUser.objects.get(id=one_user.id))
-    # api_response = api_client.get(path=reverse(router.urls[0].name), format='json')
     api_response = api_client.get(path=LIST_PATH, format='json')
-    # serializer = CustomUserSerializer(queryset, many=True)
-    # print(f'api_response.json(): {dir(api_client)}')
-    # assert api_response.status_code == HTTP_200_OK
     assert api_response.status_code == HTTP_200_OK
     # assert api_response.data == serializer.data
     # assert api_response.json() == serializer.data
@@ -118,11 +84,11 @@ def test_create():
 
 
 @pytest.mark.django_db
-def test_retrieve():
+def test_retrieve(one_user):
     api_client = APIClient()
-    auth_user = CustomUser.objects.get(id=1)
-    print(f'auth_user: {auth_user}')
-    api_client.force_authenticate(auth_user)
+    # auth_user = CustomUser.objects.get(id=1)
+    print(f'auth_user: {one_user}')
+    api_client.force_authenticate(one_user)
     api_response = api_client.get(path=RETRIEVE_PATH, format='json')
     # other_user_id = not one_user.id
     # other_user = CustomUser.objects.get(id=RETRIEVE_PATH[-1])
@@ -135,7 +101,6 @@ def test_retrieve():
     # else:
     #
     #     assert api_response.status_code == HTTP_404_NOT_FOUND
-
 
 # @pytest.mark.django_db
 # def test_filter():
