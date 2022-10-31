a19f5ed3c6caa2f3d4bd424e17b1e63cd653a715
serg <sergeysharay1987@gmail.com>
2022-10-31 18:08:30 +0300

    Fixture list_users was created, it is used in test_list and test_filter

--- my_tinder/tests/test_views.py
+++ my_tinder/tests/test_views.py
@@ -8,45 +8,40 @@ file_name = 'image.png'
 
 data = {'gender': 'М', 'last_name': 'Ivanov', 'email': 'Ivanov_1000@gmail.com', 'password': 'qwerty1000'}
 
-# queryset = baker.make('my_tinder.CustomUser', _quantity=10)
 LIST_PATH = 'http://testserver/my_tinder/list'
 FILTER_PATH = 'http://testserver/my_tinder/list?first_name=Jack'
 RETRIEVE_PATH = 'http://testserver/my_tinder/clients/1'
 CREATE_PATH = 'http://testserver/my_tinder/clients/create'
 
-# @pytest.fixture(params=[queryset])
-# def user(request):
-#     for user in request.param:
-#         print(f'user: {user}')
-#         return user
-
-
-@pytest.fixture(scope='function')
-def django_db_setup(django_db_setup, django_db_blocker):
-    with django_db_blocker.unblock():
-        baker.make('my_tinder.CustomUser', _quantity=5)
-
 
 @pytest.fixture()
 def api_client(db):
-    user = baker.make(CustomUser)
+    user = baker.make('my_tinder.CustomUser', email='auth_user@yandex.ru', first_name='auth_user')
     api_client = APIClient()
     api_client.force_authenticate(user)
     return api_client
 
 
 @pytest.fixture()
-def one_user():
-    print(f'one_user_from_fixture_one_user: {baker.make("CustomUser")}')
-    return baker.make(CustomUser)
+def list_users(db):
+    baker.make(
+        'my_tinder.CustomUser', email='jack_sparrow@gmail.com', first_name='Jack', last_name='Sparrow', gender='М'
+    )
+    baker.make('my_tinder.CustomUser', email='jack_smith@gmail.com', first_name='Jack', last_name='Smith', gender='М')
+    baker.make(
+        'my_tinder.CustomUser', email='jack_daniels@gmail.com', first_name='Jack', last_name='Daniels', gender='М'
+    )
+    baker.make(
+        'my_tinder.CustomUser', email='elizabeth_swon@gmail.com', first_name='Elizabeth', last_name='Swon', gender='Ж'
+    )
+    baker.make('my_tinder.CustomUser', email='tia_dalma@gmail.com', first_name='Tia', last_name='Dalma', gender='Ж')
+    return CustomUser.objects.all()
 
 
 @pytest.mark.django_db
-def test_list(api_client):
+def test_list(api_client, list_users):
     api_response = api_client.get(path=LIST_PATH, format='json')
     assert api_response.status_code == HTTP_200_OK
-    # assert api_response.data == serializer.data
-    # assert api_response.json() == serializer.data
 
 
 @pytest.mark.django_db
@@ -56,26 +51,17 @@ def test_create():
     assert api_response.status_code == HTTP_201_CREATED
 
 
-def test_retrieve(api_client):
-
+def test_retrieve(api_client, list_users):
     api_response = api_client.get(path=RETRIEVE_PATH, format='json')
     other_user = CustomUser.objects.get(id=RETRIEVE_PATH[-1])
     if CustomUser.objects.contains(other_user):
         assert api_response.status_code == HTTP_200_OK
-    elif not CustomUser.objects.contains(other_user):
+    elif not CustomUser.objects.contains(other_user) or CustomUser.DoesNotExist:
         assert api_response.status_code == HTTP_404_NOT_FOUND
-    # if CustomUser.objects.contains(other_user):
-    #
-    #     serilaizer = CustomUserSerializer(other_user)
-    #     assert api_response.status_code == HTTP_200_OK
-    #     assert api_response.data == serilaizer.data
-    # else:
-    #
-    #     assert api_response.status_code == HTTP_404_NOT_FOUND
 
 
 @pytest.mark.django_db
-def test_filter(api_client):
+def test_filter(api_client, list_users):
     query_params = {'first_name': 'Jack'}
     api_response = api_client.get(FILTER_PATH, query_params)
     assert api_response.status_code == HTTP_200_OK
