6aed44549069ed6e49d23e5db7a30b1d10034a98
serg <sergeysharay1987@gmail.com>
2022-11-06 23:46:24 +0300

    CUser was imported and used as parent class for CustomUser

--- my_tinder/models.py
+++ my_tinder/models.py
@@ -1,69 +1,19 @@
-from django.contrib.auth.base_user import BaseUserManager
-from django.db.models import CharField, EmailField, ImageField, ManyToManyField, TextChoices
-from django.contrib.auth.models import AbstractUser
-from django.utils.translation import gettext_lazy as _
-
-
-class CustomUserManager(BaseUserManager):
-    """
-    Пользовательский менеджер моделей, где электронная почта является уникальным идентификатором
-    для аутентификации вместо имен пользователей.
-    """
-
-    def create_user(self, email, password, **extra_fields):
-        """
-        Создаёт и сохраняет пользователя с заданным адресом электронной почты и паролем.
-        """
-        if not email:
-            raise ValueError(_('The Email must be set'))
-        email = self.normalize_email(email)
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, password, **extra_fields):
-        """
-        Создаёт и сохраняет суперпользователя с заданным адресом электронной почты и паролем.
-        """
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
-
-        if extra_fields.get('is_staff') is not True:
-            raise ValueError(_('Superuser must have is_staff=True.'))
-        if extra_fields.get('is_superuser') is not True:
-            raise ValueError(_('Superuser must have is_superuser=True.'))
-        return self.create_user(email, password, **extra_fields)
+from cuser.models import CUser
+from django.db.models import CharField, ImageField, ManyToManyField, TextChoices
 
 
 class Gender(TextChoices):
-
     MALE = 'М', 'Мужчина'
     FEMALE = 'Ж', 'Женщина'
 
 
-class CustomUser(AbstractUser):
-
-    username = None
-    email = EmailField(
-        verbose_name='Электронная почта',
-        unique=True,
-        error_messages={'unique': 'Участник с такой '
-                                  'электронной почтой '
-                                  'уже существует.'}
-    )
+class CustomUser(CUser):
 
     avatar = ImageField(verbose_name='Фото', upload_to='photos/%Y/%m/%d', blank=True, null=True)
     gender = CharField(verbose_name='Пол', max_length=1, choices=Gender.choices)
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
     liked_users = ManyToManyField('self', blank=True, symmetrical=False, related_name='liked_users_set')
     unliked_users = ManyToManyField('self', blank=True, symmetrical=False, related_name='unliked_users_set')
-    objects = CustomUserManager()
 
     class Meta:
-
         verbose_name = 'Участник'
         verbose_name_plural = 'Участники'
