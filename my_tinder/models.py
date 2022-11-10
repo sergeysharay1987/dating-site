6ae3fa797c4625128778f82be6397b919f1ccdaf
serg <sergeysharay1987@gmail.com>
2022-11-10 20:48:38 +0300

    ModelCustomUser was changed and migrations were made

--- my_tinder/models.py
+++ my_tinder/models.py
@@ -1,5 +1,6 @@
-from cuser.models import CUser
-from django.db.models import CharField, ImageField, ManyToManyField, TextChoices
+from django.db.models import CASCADE, CharField, ImageField, ManyToManyField, OneToOneField, TextChoices
+from django.db import models
+from dating_site.settings import AUTH_USER_MODEL
 
 
 class Gender(TextChoices):
@@ -7,12 +8,12 @@ class Gender(TextChoices):
     FEMALE = 'Ж', 'Женщина'
 
 
-class CustomUser(CUser):
-
+class CustomUser(models.Model):
+    user = OneToOneField(AUTH_USER_MODEL, on_delete=CASCADE)
     avatar = ImageField(verbose_name='Фото', upload_to='photos/%Y/%m/%d', blank=True, null=True)
     gender = CharField(verbose_name='Пол', max_length=1, choices=Gender.choices)
-    liked_users = ManyToManyField('self', blank=True, symmetrical=False, related_name='liked_users_set')
-    unliked_users = ManyToManyField('self', blank=True, symmetrical=False, related_name='unliked_users_set')
+    liked_users = ManyToManyField(AUTH_USER_MODEL, blank=True, symmetrical=False, related_name='liked_users_set')
+    unliked_users = ManyToManyField(AUTH_USER_MODEL, blank=True, symmetrical=False, related_name='unliked_users_set')
 
     class Meta:
         verbose_name = 'Участник'
