361f9b17fa26cde2e08892afa36612ae6dd1c3fa
serg <sergeysharay1987@gmail.com>
2022-11-12 23:15:09 +0300

    Method create was overrided in CreateUserSerializer and UserSerilizer was created

--- my_tinder/serializers.py
+++ my_tinder/serializers.py
@@ -1,15 +1,20 @@
 from io import BytesIO
+from django.contrib.auth import get_user_model
 from rest_framework.serializers import ModelSerializer
-from my_tinder.models import CustomUser
 from my_tinder.my_tinder_services.put_watermark import put_watermark
 from PIL import Image
 
 
-class CustomUserSerializer(ModelSerializer):
+class CreateUserSerializer(ModelSerializer):
 
     class Meta:
-        model = CustomUser
-        fields = ['avatar', 'email', 'gender', 'first_name', 'last_name']
+        model = get_user_model()
+        fields = ['avatar', 'email', 'gender', 'first_name', 'last_name', 'password']
+
+    def create(self, validated_data):
+        obj = get_user_model().objects.create_user(**validated_data)
+        obj.save()
+        return obj
 
     def save(self, **kwargs):
         if self.validated_data.get('avatar'):
@@ -17,3 +22,10 @@ class CustomUserSerializer(ModelSerializer):
             watermarked_image: BytesIO = put_watermark(base_image)
             self.validated_data['avatar'].file = watermarked_image
         return super().save()
+
+
+class UserSerializer(ModelSerializer):
+
+    class Meta:
+        model = get_user_model()
+        fields = ['avatar', 'email', 'gender', 'first_name', 'last_name']
