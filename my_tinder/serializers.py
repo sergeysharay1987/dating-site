529faf395e4d072f5b863a2ed5bcbe5c4835b864
serg <sergeysharay1987@gmail.com>
2022-11-16 10:51:24 +0300

    CreateUserSerializer was changed

--- my_tinder/serializers.py
+++ my_tinder/serializers.py
@@ -1,30 +1,27 @@
 from io import BytesIO
 from django.contrib.auth import get_user_model
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import CharField, ModelSerializer
 from my_tinder.my_tinder_services.put_watermark import put_watermark
 from PIL import Image
 
 
 class CreateUserSerializer(ModelSerializer):
+    gender = CharField(required=True)
 
     class Meta:
         model = get_user_model()
         fields = ['avatar', 'email', 'gender', 'first_name', 'last_name', 'password']
-
-    def create(self, validated_data):
-        obj = get_user_model().objects.create_user(**validated_data)
-        obj.save()
-        return obj
 
     def save(self, **kwargs):
         if self.validated_data.get('avatar'):
             base_image: Image = Image.open(self.validated_data['avatar'].file)
             watermarked_image: BytesIO = put_watermark(base_image)
             self.validated_data['avatar'].file = watermarked_image
-        return super().save()
+
+        return super().save(**kwargs)
 
 
-class UserSerializer(ModelSerializer):
+class UserDetailSerializer(ModelSerializer):
 
     class Meta:
         model = get_user_model()
